7754398afdfdd98aecebf035db854b49faf1f7b8
Hedonihilist <zenghao_mail@163.com>
2021-09-30 12:54:23 +0800

    It's working!

--- README.md
+++ README.md
@@ -1,22 +1,72 @@
 # nu_plugin_df
 A `df` plugin for Nushell
 
-
 ## install
 
-TODO
+Clone this repository:
+```shell
+$ git clone git@github.com:hedonihilist/nu_plugin_df.git
+```
+
+In the repository directory, install the plugin:
+```shell
+$ cargo install --path .
+```
+
+The above command will install `nu_plugin_df` to `~/.cargo/bin/`, make sure you have `~/.cargo/bin` in the `nu` plugin dir list.
+You can check it out in `nu` by:
+```shell
+> config get plugin_dirs
+/home/z/.cargo/bin
+```
+
+You can add plugin dir in `nu` by (change `<HOME_DIR>` to your absolute home dir):
+```shell
+> config set plugin_dirs ["<HOME_DIR>/.cargo/bin"]
+```
 
 ## usage in `nu`
 
-Show file systems:
+### Simple Usage
+
 ```shell
 > df
+/home/z> df
+────┬────────────┬──────────┬──────────┬─────────┬──────────┬──────────┬───────────┬───────────┬───────────┬──────────┬───────────────────────────────────────────────────────────────────────────────────────────
+ #  │ Filesystem │   Type   │  Inodes  │  IUsed  │  IFree   │  IUse%   │   Total   │   Used    │   Avail   │   Use%   │                                        mount-point                                        
+────┼────────────┼──────────┼──────────┼─────────┼──────────┼──────────┼───────────┼───────────┼───────────┼──────────┼───────────────────────────────────────────────────────────────────────────────────────────
+  0 │ dev        │ devtmpfs │  2038116 │     593 │  2037523 │   0.0290 │   7.8 GiB │       0 B │   7.8 GiB │   0.0000 │ /dev                                                                                      
+  1 │ run        │ tmpfs    │  2040517 │    1150 │  2039367 │   0.0563 │   7.8 GiB │   1.9 MiB │   7.8 GiB │   0.0235 │ /run/snapd/ns                                                                             
+  2 │ /dev/sda5  │ ext4     │  6250496 │  804929 │  5445567 │  12.8778 │  93.4 GiB │  60.6 GiB │  32.8 GiB │  64.8701 │ /                                                                                         
+  3 │ tmpfs      │ tmpfs    │  2040517 │     909 │  2039608 │   0.0445 │   7.8 GiB │ 536.7 MiB │   7.3 GiB │   6.7329 │ /dev/shm                                                                                  
+  4 │ tmpfs      │ tmpfs    │   409600 │     468 │   409132 │   0.1142 │   7.8 GiB │ 137.9 MiB │   7.6 GiB │   1.7305 │ /tmp                                                                                      
+  5 │ /dev/loop0 │ squashfs │    10803 │   10803 │        0 │ 100.0000 │  55.5 MiB │  55.5 MiB │       0 B │ 100.0000 │ /var/lib/snapd/snap/core18/2128                                                           
+  6 │ /dev/loop2 │ squashfs │    64986 │   64986 │        0 │ 100.0000 │  65.1 MiB │  65.1 MiB │       0 B │ 100.0000 │ /var/lib/snapd/snap/gtk-common-themes/1515                                                
+  7 │ /dev/loop1 │ squashfs │    27806 │   27806 │        0 │ 100.0000 │ 164.9 MiB │ 164.9 MiB │       0 B │ 100.0000 │ /var/lib/snapd/snap/gnome-3-28-1804/161                                                   
+  8 │ /dev/loop3 │ squashfs │       29 │      29 │        0 │ 100.0000 │ 128.0 KiB │ 128.0 KiB │       0 B │ 100.0000 │ /var/lib/snapd/snap/bare/5                                                                
+  9 │ /dev/loop4 │ squashfs │      474 │     474 │        0 │ 100.0000 │  32.4 MiB │  32.4 MiB │       0 B │ 100.0000 │ /var/lib/snapd/snap/snapd/12883                                                           
+ 10 │ /dev/loop5 │ squashfs │      132 │     132 │        0 │ 100.0000 │  70.6 MiB │  70.6 MiB │       0 B │ 100.0000 │ /var/lib/snapd/snap/superproductivity/1360                                                
+ 11 │ /dev/loop7 │ squashfs │      474 │     474 │        0 │ 100.0000 │  32.4 MiB │  32.4 MiB │       0 B │ 100.0000 │ /var/lib/snapd/snap/snapd/13170                                                           
+ 12 │ /dev/loop6 │ squashfs │    65095 │   65095 │        0 │ 100.0000 │  65.2 MiB │  65.2 MiB │       0 B │ 100.0000 │ /var/lib/snapd/snap/gtk-common-themes/1519                                                
+ 13 │ /dev/sda6  │ ext4     │  6250496 │ 1377433 │  4873063 │  22.0371 │  93.4 GiB │  84.6 GiB │   8.7 GiB │  90.6399 │ /home                                                                                     
+ 14 │ /dev/sda8  │ fuseblk  │ 53512420 │   25929 │ 53486491 │   0.0484 │ 100.0 GiB │  49.0 GiB │  51.0 GiB │  49.0295 │ /home/z/share                                                                             
+ 15 │ /dev/sda1  │ vfat     │        0 │       0 │        0 │   0.0000 │ 299.8 MiB │  32.0 MiB │ 267.8 MiB │  10.6881 │ /boot/efi                                                                                 
+ 16 │ /dev/sda7  │ ext4     │  5005312 │     305 │  5005007 │   0.0060 │  74.6 GiB │  13.7 GiB │  60.9 GiB │  18.3452 │ /home/trusted                                                                             
+ 17 │ tmpfs      │ tmpfs    │   408103 │     198 │   407905 │   0.0485 │   1.6 GiB │ 212.0 KiB │   1.6 GiB │   0.0129 │ /run/user/1000                                                                            
+ 18 │ overlay    │ overlay  │  6250496 │  804929 │  5445567 │  12.8778 │  93.4 GiB │  60.6 GiB │  32.8 GiB │  64.8701 │ /var/lib/docker/overlay2/ce8223387557f93a71985cc1d0a6d4cadef3a3220a0c15ab3944671353d7f8e0 
+    │            │          │          │         │          │          │           │           │           │          │ /merged                                                                                   
+────┴────────────┴──────────┴──────────┴─────────┴──────────┴──────────┴───────────┴───────────┴───────────┴──────────┴───────────────────────────────────────────────────────────────────────────────────────────
+
 ```
+
+### Show all file systems
 
 Show all file systems, including pseudo, duplicate, inaccessible file systems
 ```shell
 > df -a
 ```
+
+### Show local file systems only
 
 Show local file systems only
 ```shell
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,40 @@
 mod mountinfo;
 
+use crate::mountinfo::get_mountinfo_list;
+use mountinfo::MountInfo;
+use nix::sys::statvfs::Statvfs;
 use nu_errors::ShellError;
 use nu_plugin::{serve_plugin, Plugin};
 use nu_protocol::{
-    CallInfo, Dictionary, ReturnSuccess, ReturnValue, Signature, SyntaxShape, UntaggedValue, Value,
+    CallInfo, Dictionary, ReturnSuccess, ReturnValue, Signature, UntaggedValue, Value,
 };
+use std::collections::{HashMap, HashSet};
+
+#[derive(Default, Debug)]
+pub struct Options {
+    pub show_local_fs: bool,
+    pub show_all_fs: bool,
+    pub listed_fs: HashSet<String>,
+    pub excluded_fs: HashSet<String>,
+    pub human_readable: bool,
+    pub print_grand_total: bool,
+    pub field_list: Vec<String>,
+    pub human_readable_1024: bool, // true => show size in powers of 1024, false => powers of 1000
+    pub inodes: bool,
+    pub show_fs_type: bool,
+    pub output_all_fields: bool,
+}
+
+impl Options {
+    pub fn new() -> Options {
+        Options {
+            show_local_fs: true,
+            show_all_fs: false,
+            human_readable: true,
+            ..Default::default()
+        }
+    }
+}
 
 struct Df;
 
@@ -16,21 +46,167 @@ impl Plugin for Df {
                     "all", "Show all file systems, including include pseudo, duplicate, inaccessible file systems", Some('a')
                 )
                 .switch(
-                    "local", "Show local file systems only.", Some('l'))
+                    "local", "Show local file systems only.", Some('l')).filter()
         )
     }
 
     fn begin_filter(&mut self, call_info: CallInfo) -> Result<Vec<ReturnValue>, ShellError> {
-        Ok(vec![ReturnSuccess::value(Value::new(
-            UntaggedValue::Row(Dictionary::from(call_info.args.named.unwrap())),
-            call_info.name_tag,
-        ))])
+        let mut options = Options::new();
+        if let Some(args) = call_info.args.named {
+            options.show_all_fs = args.get("all").is_some();
+            options.show_local_fs = args.get("local").is_some();
+        }
+        let value_list = get_all_entries(&options);
+        Ok(value_list
+            .into_iter()
+            .map(|v| ReturnSuccess::value(v))
+            .collect())
     }
 
     fn filter(&mut self, _input: Value) -> Result<Vec<ReturnValue>, ShellError> {
-        unimplemented!()
+        Ok(vec![])
     }
 }
+
+fn get_all_entries(options: &Options) -> Vec<UntaggedValue> {
+    let mut rows: Vec<UntaggedValue> = vec![];
+
+    let mountinfo_list = filter_mountinfo_list(get_mountinfo_list(), &options);
+
+    for mount in mountinfo_list.into_iter() {
+        if let Some(stat) = get_dev(&mount, options) {
+            let mut dict = Dictionary::default();
+
+            dict.insert(
+                "Filesystem".into(),
+                UntaggedValue::from(mount.mount_source.to_string()).into(),
+            );
+            dict.insert(
+                "Type".into(),
+                UntaggedValue::from(mount.fs_type.to_string()).into(),
+            );
+
+            // inodes
+            let itotal = stat.files();
+            let ifree = stat.files_available();
+            let iused = itotal - ifree;
+            // cast to i64 to get proper alignment
+            dict.insert("Inodes".into(), UntaggedValue::int(itotal as i64).into());
+            dict.insert("IUsed".into(), UntaggedValue::int(iused as i64).into());
+            dict.insert("IFree".into(), UntaggedValue::int(ifree as i64).into());
+            let ipercent: f64 = match itotal != 0 {
+                true => 100f64 * (iused as f64) / (ifree + iused) as f64,
+                false => 0f64,
+            };
+            dict.insert("IUse%".into(), UntaggedValue::from(ipercent).into());
+
+            // size
+            // in B
+            let total = stat.blocks() * stat.fragment_size();
+            let free = stat.blocks_available() * stat.fragment_size();
+            let used = total - free;
+            dict.insert("Total".into(), UntaggedValue::filesize(total).into());
+            dict.insert("Used".into(), UntaggedValue::filesize(used).into());
+            dict.insert("Avail".into(), UntaggedValue::filesize(free).into());
+            let percent: f64 = match total != 0 {
+                true => 100f64 * (used as f64) / (free + used) as f64,
+                false => 0f64,
+            };
+            dict.insert("Use%".into(), UntaggedValue::from(percent).into());
+            dict.insert(
+                "mount-point".into(),
+                UntaggedValue::filepath(mount.mount_point.to_string()).into(),
+            );
+            rows.push(UntaggedValue::Row(dict));
+        }
+    }
+    rows
+}
+
+fn get_dev(mount: &MountInfo, options: &Options) -> Option<Statvfs> {
+    if mount.is_remote() && options.show_local_fs {
+        return None;
+    }
+    if mount.is_dummy() && !options.show_all_fs && options.listed_fs.is_empty() {
+        return None;
+    }
+    // fs_type not listed
+    if !options.listed_fs.is_empty() && !options.listed_fs.contains(&mount.fs_type)
+        || options.excluded_fs.contains(&mount.fs_type)
+    {
+        return None;
+    }
+
+    let res_stat = nix::sys::statvfs::statvfs::<str>(mount.mount_point.as_ref());
+
+    if res_stat.is_err() {
+        return None;
+    }
+    let stat = res_stat.unwrap();
+
+    if stat.blocks() == 0 && !options.show_all_fs && options.listed_fs.is_empty() {
+        return None;
+    }
+
+    Some(stat)
+}
+
+fn filter_mountinfo_list(list: Vec<MountInfo>, options: &Options) -> Vec<MountInfo> {
+    let mut filtered: Vec<MountInfo> = vec![];
+    let mut seen: HashMap<u64, usize> = HashMap::new();
+    for me in list.into_iter() {
+        let mut discard_me: Option<usize> = None; //
+                                                  // skip
+        if (me.is_remote() && options.show_local_fs)
+            || (me.is_dummy() && !options.show_all_fs && !options.listed_fs.contains(&me.fs_type))
+            || (!options.listed_fs.is_empty() && !options.listed_fs.contains(&me.fs_type))
+            || options.excluded_fs.contains(&me.fs_type)
+        {
+            // pass
+        } else {
+            /*
+            在Linux中有一个bind mount的概念，能把一个目录挂载到另一个目录下，例如mount -o bind /boot/efi /tmp/bindmount
+            在df的输出中我们希望去除重复的设备
+             */
+            if let Some(&idx) = seen.get(&me.dev()) {
+                let seen_dev: &MountInfo = &filtered[idx];
+
+                // target指当前me
+                // source指
+                let target_nearer_root = seen_dev.mount_point.len() > me.mount_point.len();
+                let source_below_root = !seen_dev.root.is_empty()
+                    && !me.root.is_empty()
+                    && (seen_dev.root.len() < me.root.len());
+                if !options.print_grand_total
+                    && me.is_remote()
+                    && seen_dev.is_remote()
+                    && seen_dev.mount_source.eq(&me.mount_source)
+                {
+                    // don't discard
+                } else if (me.mount_source.contains('/') && !seen_dev.mount_source.contains('/'))
+                    || (target_nearer_root && !source_below_root)
+                    || (!seen_dev.mount_source.eq(&me.mount_source)
+                        && seen_dev.mount_point.eq(&me.mount_point))
+                {
+                    // discard this one
+                    continue;
+                } else {
+                    discard_me = Some(idx);
+                }
+            }
+        }
+        if let Some(discard_idx) = discard_me {
+            filtered[discard_idx] = me;
+            //std::mem::replace(&mut filtered[discard_idx], me);
+        } else {
+            let dev = me.dev();
+            filtered.push(me);
+            seen.insert(dev, filtered.len() - 1);
+        }
+    }
+    filtered
+}
+
 fn main() {
     serve_plugin(&mut Df {});
 }
